7d0be924666831073f9b6f483dfaa97df52c63d5
Jose Fernando U <44246337+fercho2000@users.noreply.github.com>
2022-09-06 22:46:41 -0500

    first commit

---
+++ src/servidor_socket/ManejadorArchivoTxt.java
@@ -0,0 +1,47 @@
+package servidor_socket;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+// a. Monte el compilador de Java. Cree un archivo llamado datos.txt usando la utilidad manejo de archivos en Java.
+public class ManejadorArchivoTxt {
+
+    private static final String NOMBRE_DEL_ARCHIVO = "\\src\\archivos\\datos.txt";
+
+    public ManejadorArchivoTxt() {
+
+    }
+
+    public void crearArchivo() {
+        try {
+            if (validarSiExisteArchivo()) {
+                eliminarArchivo();
+                System.out.println("Se elimino archivo anterior..");
+            }
+            Files.createFile(Paths.get((obtenerRutaArchivo())));
+            System.out.println("Archivo creado...");
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error creando el archivo...");
+        }
+    }
+
+    boolean validarSiExisteArchivo() {
+        try {
+            return Files.exists(Paths.get((obtenerRutaArchivo())));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void eliminarArchivo() throws IOException {
+        File targetFile = new File(String.valueOf(obtenerRutaArchivo()));
+        targetFile.delete();
+    }
+
+    String obtenerRutaArchivo() throws IOException {
+        return Paths.get("").toAbsolutePath() + NOMBRE_DEL_ARCHIVO;
+    }
+}
